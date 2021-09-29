b369d4bc8b6e8c7937f64bac6d767fe0f6c6690a
Anmol <anmol@anmol.local>
2021-09-29 19:48:42 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "anmol.one",
   },
-  plugins: [],
+  plugins: [
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 //Step 1: Import
 import * as React from 'react'
-//import { Link } from 'gatsby';
+//import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this the layout in React</p>
+    return (
+      <Layout pageTitle="Home Page">
+        <p>I'm making this the layout in React</p>
+        <StaticImage
+        alt="Myntra's Case-Study by Obvious.in"
+        src="../images/myntra.webp"
+        />
     </Layout>
   )
 }
